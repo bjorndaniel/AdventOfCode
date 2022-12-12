34b6dd37eca12a38a4458ab12b882651c956706f
Daniel Sandberg <bjorndaniel@gmail.com>
2022-12-12 17:58:42 +0100

    Broken dijkstra

---
+++ 2022/2022.Tests/Day12Tests.cs
@@ -0,0 +1,72 @@
+﻿namespace AoC2022.Tests;
+public class Day12Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day12Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day12-test.txt";
+
+        //When
+        var (matrix, start, end) = Day12.ParseInput(filename);
+
+        //Then
+        Assert.True(start.X == 0 && start.Y == 0, $"Expected start to be 0,0, got {start.X},{start.Y}");
+        Assert.True(end.X == 5 && end.Y == 2, $"Expected start to be 0,0, got {start.X},{start.Y}");
+        Assert.True(matrix[0, 0] == 'S', $"Expected S, got {matrix[0, 0]}");
+        Assert.True(matrix[4, 1] == 'b', $"Expected b, got {matrix[4, 1]}");
+        Assert.True(matrix[4, 2] == 'd', $"Expected d, got {matrix[4, 2]}");
+        Assert.True(matrix[2, 5] == 'E', $"Expected E, got {matrix[2, 5]}");
+        Assert.True(matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1] == 'i', $"Expected i, got {matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1]}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day12-test.txt";
+
+        //When
+        var (result, matrix) = Day12.SolvePart1(filename);
+
+        //Then
+        Print(matrix);
+        Assert.True(31 == result, $"Expected 31, got {result}");
+    }
+
+    private void Print(int[,] matrix)
+    {
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            var sb = new StringBuilder();
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                if (row == 0 && col == 0)
+                {
+                    sb.Append("  S");
+                }
+                else if (row == 2 && col == 5)
+                {
+                    sb.Append("  E");
+                }
+                else if (matrix[row, col] > 99)
+                {
+                    sb.Append("  m");
+                }
+                else
+                {
+                    sb.Append(matrix[row, col].ToString().PadLeft(3, ' '));
+                }
+            }
+            _output.WriteLine(sb.ToString());
+        }
+        _output.WriteLine("");
+    }
+}
---
+++ 2022/2022/Day12.cs
@@ -0,0 +1,76 @@
+﻿namespace AoC2022;
+public static class Day12
+{
+    private static int[] _dCol = { -1, 0, 1, 0 };
+    private static int[] _dRow = { 0, 1, 0, -1 };
+
+    public static (char[,], Point start, Point end) ParseInput(string filename)
+    {
+        Point start = new Point(-1, -1);
+        Point end = new Point(-1, -1);
+        var lines = File.ReadAllLines(filename);
+        var result = new char[lines.Length, lines[0].Length];
+        for (int row = 0; row < lines.Length; row++)
+        {
+            for (int col = 0; col < lines[row].Length; col++)
+            {
+                if (lines[row][col] == 'S')
+                {
+                    start = new Point(col, row);
+                }
+                if (lines[row][col] == 'E')
+                {
+                    end = new Point(col, row);
+                }
+                result[row, col] = lines[row][col];
+            }
+        }
+        return (result, start, end);
+    }
+    public static (int result, int[,] visited) SolvePart1(string filename)
+    {
+        var (matrix, s, e) = ParseInput(filename);
+        return DijkstraWithObstacles(matrix, s, e);
+    }
+
+    public static (int result, int[,] visited) DijkstraWithObstacles(char[,] matrix, Point start, Point end)
+    {
+        var distances = new int[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int y = 0; y < matrix.GetLength(0); y++)
+        {
+            for (int x = 0; x < matrix.GetLength(1); x++)
+            {
+                distances[y, x] = int.MaxValue;
+            }
+        }
+        distances[start.Y, start.X] = 0;
+        var queue = new Queue<(int y, int x)>();
+        queue.Enqueue((start.Y, start.X));
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            if (current.y == end.Y && current.x == end.X)
+            {
+                return (distances[current.y, current.x], distances);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                var (nextx, nexty) = (current.x + _dCol[i], current.y + _dRow[i]);
+                if (nextx < 0 || nextx >= matrix.GetLength(1) || nexty < 0 || nexty >= matrix.GetLength(0))
+                {
+                    continue;
+                }
+                var canMove = (current.x == start.X && current.y == start.Y) || matrix[nexty, nextx] - matrix[current.y, current.x] < 2;
+                if (distances[nexty, nextx] > distances[current.y, current.x] + 1 && canMove)
+                {
+                    distances[nexty, nextx] = distances[current.y, current.x] + 1;
+                    queue.Enqueue((nexty, nextx));
+                }
+            }
+        }
+        return (distances[end.Y, end.X], distances);
+
+    }
+
+}
+
