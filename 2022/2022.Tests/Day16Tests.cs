b6b5ae87729b68b7e77220150d369a2b42b3f6db
Daniel Sandberg <bjorndaniel@gmail.com>
2022-12-16 23:15:09 +0100

    Started day 16

---
+++ 2022/2022.Tests/Day16Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace AoC2022.Tests;
+public class Day16Tests
+{
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day16-test.txt";
+
+        //When
+        var result = Day16.ParseInput(filename);
+
+        //Then
+        Assert.True(10 == result.Count(), $"Expected 10 valves, got {result.Count()}");
+        var first = result.First(_ => _.Name == "AA");
+        Assert.True(0 == first.FlowRate, $"Expected {20}, got {first.FlowRate}");
+        Assert.True(3 == first.Adjacent.Count(), $"Expected {3}, got {first.Adjacent.Count()}");
+        Assert.Contains(first.Adjacent, _ => _.Name == "DD" && _.FlowRate == 20);
+    }
+}
---
+++ 2022/2022/Day16.cs
@@ -0,0 +1,58 @@
+﻿namespace AoC2022;
+public static class Day16
+{
+    public static IEnumerable<Valve> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var result = new List<Valve>();
+        foreach (var l in lines)
+        {
+            var name = l
+                .Replace("Valve ", "")
+                .Split(' ')[0];
+            var flowRate = l[(l.IndexOf('=') + 1)..l.IndexOf(';')];
+            var valves = l[(l.IndexOf("valves ") + 7)..]
+                .Split(',')
+                .Select(_ => _.Trim());
+            var valve = new Valve(
+                name, int.Parse(flowRate),
+                valves.Select(_ => new Valve(_, 0, new List<Valve>())).ToList()
+            );
+            result.Add(valve);
+            //Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        }
+        foreach (var v in result)
+        {
+            var adjacent = v.Adjacent
+                .Select(_ => _.Name)
+                .ToList();
+            var correct = result
+                .Where(_ => adjacent.Contains(_.Name))
+                .ToList();
+            v.Adjacent = correct;
+            yield return v;
+        }
+    }
+
+    public static int SolvePart1(string filename)
+    {
+        var valves = ParseInput(filename);
+        var result = 0;
+
+        return result;
+    }
+}
+
+public class Valve
+{
+    public Valve(string name, int flowRate, List<Valve> adjacent)
+    {
+        Name = name;
+        FlowRate = flowRate;
+        Adjacent = adjacent;
+    }
+
+    public string Name { get; }
+    public int FlowRate { get; }
+    public List<Valve> Adjacent { get; set; }
+}
