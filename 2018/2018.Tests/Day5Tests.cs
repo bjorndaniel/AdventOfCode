a0a8405bdffc28540df04c4632385bbf2fa70d81
Daniel Sandberg <bjorndaniel@gmail.com>
2023-11-26 16:08:43 +0100

    Day5

---
+++ 2018/2018.Tests/Day5Tests.cs
@@ -0,0 +1,53 @@
+﻿namespace AoC2018.Tests
+{
+    public class Day5Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Day5Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Can_parse_input()
+        {
+            //Given
+            var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+            //When
+            var result = Day5.ParseInput(filename);
+
+            //Then
+            Assert.True("dabAcCaCBAcCcaDA" == result, $"Expected result to be dabAcCaCBAcCcaDA but was {result}");
+            
+        }
+
+        [Fact]
+        public void Can_solve_part1_for_test()
+        {
+            //Given
+            var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+            //When
+            var result = Day5.Part1(filename, new TestPrinter(_output));
+
+            //Then
+            Assert.True("10" == result.Result, $"Expected result to be 10 but was {result.Result}");
+        }
+
+        [Fact]
+        public void Can_solve_part2_for_test()
+        {
+            //Given
+            var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+            //When
+            var result = Day5.Part2(filename, new TestPrinter(_output));
+
+            //Then
+            Assert.True("4" == result.Result, $"Expected result to be 4 but was {result.Result}");
+
+        }
+    }
+}
---
+++ 2018/2018/Day5.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC2018;
+public class Day5
+{
+    public static string ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return lines[0];
+    }
+
+    [Solveable("2018/Puzzles/Day5.txt", "Day5 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var input = ParseInput(filename);
+        input = Reduce(input);
+        return new SolutionResult(input.Length.ToString());
+    }
+
+    private static string Reduce(string input)
+    {
+        var didRemove = true;
+        while (didRemove)
+        {
+            var current = input.Length;
+            for (var i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] != input[i + 1] && (input[i] == char.ToLower(input[i + 1]) || char.ToLower(input[i]) == input[i + 1]))
+                {
+                    input = input.Remove(i, 2);
+                }
+            }
+            if (current == input.Length)
+            {
+                didRemove = false;
+            }
+        }
+
+        return input;
+    }
+
+    [Solveable("2018/Puzzles/Day5.txt", "Day5 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var alphabetArray = new List<(char, char)>();
+        for (int i = 0; i < 26; i++)
+        {
+            alphabetArray.Add(((char)(97 + i), (char)(65 + i)));
+        }
+        var input = ParseInput(filename);
+        var shortest = int.MaxValue;
+        foreach(var tuple in alphabetArray)
+        {
+            var c = tuple.Item1;
+            var c2 = tuple.Item2;
+            var newInput = input.Replace(c.ToString(), "").Replace(c2.ToString(), "");
+            var reduced = Reduce(newInput);
+            if (reduced.Length < shortest)
+            {
+                shortest = reduced.Length;
+            }
+        }
+        return new SolutionResult(shortest.ToString());
+    }
+
+}
