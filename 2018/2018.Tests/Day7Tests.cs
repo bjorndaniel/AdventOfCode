91e68f4a5f1314e272456c0bd26fcd8ad69c36fd
Daniel Sandberg <bjorndaniel@gmail.com>
2023-11-28 11:07:47 +0100

    Day 7 part 1

---
+++ 2018/2018.Tests/Day7Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC2018.Tests;
+public class Day7Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day7Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+
+        //When
+        var result = Day7.ParseInput(filename);
+
+        //Then
+        Assert.True(7 == result.Count, $"Expected 7 but was {result.Count}");
+        Assert.True('C' == result.First().step, $"Expected C but was {result.First().step}");
+        Assert.True('A' == result.First().neighbor, $"Expected A but was {result.First().neighbor}");
+        Assert.True('F' == result.Last().step, $"Expected F but was {result.Last().step}");
+        Assert.True('E' == result.Last().neighbor, $"Expected E but was {result.Last().neighbor}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+
+        //When
+        var result = Day7.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("CABDFE" == result.Result, $"Expected CABDFE but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test2.txt";
+
+        //When
+        var result = Day7.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True(false);
+    }
+
+}
---
+++ 2018/2018/Day7.cs
@@ -0,0 +1,76 @@
+﻿namespace AoC2018;
+public class Day7
+{
+    public static List<(char step, char neighbor)> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var result = new List<(char step, char neighbor)>();
+        foreach(var line in lines) 
+        {
+            var step = line.Split(' ')[1][0];
+            var neighbor = line.Split(' ')[7][0];
+            result.Add((step, neighbor));
+        }
+        return result;
+    }
+
+    [Solveable("2018/Puzzles/Day7.txt", "Day 7 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var dependencies = ParseInput(filename);
+        var sortedTasks = TopologicalSort(dependencies);
+
+
+        return new SolutionResult(string.Join("", sortedTasks));
+    }
+
+    [Solveable("2018/Puzzles/Day7.txt", "Day 7 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        return new SolutionResult("");
+    }
+
+    static List<char> TopologicalSort(List<(char step, char neighbor)> dependencies)
+    {
+        var nodes = new HashSet<char>();
+        var edges = new Dictionary<char, List<char>>();
+        var incomingEdges = new Dictionary<char, int>();
+        foreach (var dependency in dependencies)
+        {
+            nodes.Add(dependency.step);
+            nodes.Add(dependency.neighbor);
+            if (!edges.ContainsKey(dependency.step))
+            {
+                edges[dependency.step] = new List<char>();
+            }
+            edges[dependency.step].Add(dependency.neighbor);
+            if (!incomingEdges.ContainsKey(dependency.neighbor))
+            {
+                incomingEdges[dependency.neighbor] = 0;
+            }
+            incomingEdges[dependency.neighbor]++;
+        }
+
+        var result = new List<char>();
+        while (nodes.Any())
+        {
+            var node = nodes.Where(n => !incomingEdges.ContainsKey(n)).OrderBy(n => n).First();
+            nodes.Remove(node);
+            result.Add(node);
+
+            if (edges.ContainsKey(node))
+            {
+                foreach (var neighbor in edges[node])
+                {
+                    incomingEdges[neighbor]--;
+                    if (incomingEdges[neighbor] == 0)
+                    {
+                        incomingEdges.Remove(neighbor);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+}
