aac99310d396637382ef2acfd85ea50e8607f547
Daniel Sandberg <bjorndaniel@gmail.com>
2023-11-30 14:23:07 +0100

    Day 8

---
+++ 2018/2018.Tests/Day8Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC2018.Tests;
+public class Day8Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day8Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.ParseInput(filename);
+
+        //Then
+        Assert.True(1 == result.Count(), $"Expected 1 but was {result.Count}");
+        Assert.True("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2" == result.First(), $"Expected 2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2 but was {result.First()}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("138" == result.Result, $"Expected 138 but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("66" == result.Result, $"Expected 66 but was {result.Result}");
+    }
+
+}
---
+++ 2018/2018/Day8.cs
@@ -0,0 +1,83 @@
+﻿using static AoC2018.Day8;
+
+namespace AoC2018;
+public class Day8
+{
+    public static List<string> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return lines.ToList();
+    }
+
+    [Solveable("2018/Puzzles/Day8.txt", "Day 8 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var input = ParseInput(filename).First();
+        var data = new Queue<int>(Array.ConvertAll(input.Split(' '), int.Parse));
+        var result = ParseTree(data);
+        return new SolutionResult(SumMetadata(result).ToString());
+
+        int SumMetadata(Node node)
+        {
+            return node.Metadata.Sum() + node.Children.Sum(child => SumMetadata(child));
+        }
+    }
+
+    [Solveable("2018/Puzzles/Day8.txt", "Day 8 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var input = ParseInput(filename).First();
+        var data = new Queue<int>(Array.ConvertAll(input.Split(' '), int.Parse));
+        var result = ParseTree(data);
+
+        return new SolutionResult(GetValue(result).ToString());
+    }
+
+    private static Node ParseTree(Queue<int> data)
+    {
+        var childCount = data.Dequeue();
+        var metadataCount = data.Dequeue();
+        var children = new List<Node>();
+        var metadata = new List<int>();
+
+        for (int i = 0; i < childCount; i++)
+        {
+            children.Add(ParseTree(data));
+        }
+        for (int i = 0; i < metadataCount; i++)
+        {
+            metadata.Add(data.Dequeue());
+        }
+        return new Node(children, metadata);
+    }
+
+    private static int GetValue(Node node)
+    {
+        if(node.Children.Count == 0)
+        {
+            return node.Metadata.Sum();
+        }
+        var sum = 0;
+        foreach (var metadata in node.Metadata) 
+        {
+            if(node.Children.Count >= metadata)
+            {
+                sum += GetValue(node.Children[metadata - 1]);
+            }
+        }
+        return sum;
+    }
+
+
+    public class Node
+    {
+        public List<Node> Children { get; set; }
+        public List<int> Metadata { get; set; }
+
+        public Node(List<Node> children, List<int> metadata)
+        {
+            Children = children;
+            Metadata = metadata;
+        }
+    }
+}
