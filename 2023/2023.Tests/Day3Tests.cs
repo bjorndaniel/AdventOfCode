6802914c54cee7f672a462923dd01b39ab044ecd
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-03 13:48:37 +0100

    2023 Day 3

---
+++ 2023/2023.Tests/Day3Tests.cs
@@ -0,0 +1,60 @@
+﻿namespace AoC2023.Tests;
+public class Day3Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day3Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day3-test.txt";
+
+        //When
+        var result = Day3.ParseInput(filename);
+
+        //Then
+        Assert.True(10 == result.Parts.Count, $"Expected 10 but was {result.Parts.Count}");
+        Assert.True(6 == result.Symbols.Count, $"Expected 6 but was {result.Symbols.Count}");
+        Assert.True(3 == result.Parts.First(_ => _.Value == 467).Width, $"Expected 3 but was {result.Parts.First(_ => _.Value == 467).Width}");
+        Assert.True(0 == result.Parts.First(_ => _.Value == 467).X, $"Expected 0 but was {result.Parts.First(_ => _.Value == 467).X}");
+        Assert.True(2 == result.Parts.First(_ => _.Value == 35).Width, $"Expected 2 but was {result.Parts.First(_ => _.Value == 35).Width}");
+        Assert.True(2 == result.Parts.First(_ => _.Value == 35).Y, $"Expected 0 but was {result.Parts.First(_ => _.Value == 35).Y}");
+        Assert.True(3 == result.Symbols.Count(_ => _.C == '*'), $"Expected 3 but was {result.Symbols.Count(_ => _.C == '*')}");
+        Assert.True(6 == result.Symbols.First(_ => _.C == '#').X, $"Expected 6 but was {result.Symbols.First(_ => _.C == '#').X}");
+        Assert.True(3 == result.Symbols.First(_ => _.C == '#').Y, $"Expected 3 but was {result.Symbols.First(_ => _.C == '#').Y}");
+    }
+
+    [Theory]
+    [InlineData("Day3-test.txt", "4361")]
+    [InlineData("Day3-test2.txt", "413")]
+    public void Can_solve_part1_for_test(string input, string expected)
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}{input}";
+
+        //When
+        var result = Day3.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True(expected == result.Result, $"Expected {expected} but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day3-test.txt";
+
+        //When
+        var result = Day3.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("467835" == result.Result, $"Expected 467835 but was {result.Result}");
+    }
+
+}
---
+++ 2023/2023/Day3.cs
@@ -0,0 +1,87 @@
+﻿namespace AoC2023;
+public class Day3
+{
+    public static Schematic ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var schematic = new Schematic(new List<Part>(), new List<Symbol>());
+        for (int y = 0; y < lines.Length; y++)
+        {
+            var currentNumber = "";
+            var width = 0;
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                var c = lines[y][x];
+                if(char.IsNumber(c))
+                {
+                    currentNumber += c;
+                    width++;
+                }
+                else
+                {
+                    if (int.TryParse(currentNumber, out var number))
+                    {
+                        schematic.Parts.Add(new Part(x - width, y, width, number));
+                        currentNumber = "";
+                        width = 0;
+                    }
+                    if (c != '.')
+                    {
+                        schematic.Symbols.Add(new Symbol(c, x, y, new List<Part>()));
+                    }
+                }
+            }
+            if (int.TryParse(currentNumber, out var remaining))
+            {
+                schematic.Parts.Add(new Part(lines[y].Length - width, y, width, remaining));
+            }
+        }
+        return schematic;
+    }
+
+    [Solveable("2023/Puzzles/Day3.txt", "Day 3 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var schematic = ParseInput(filename);   
+        var adjacent = schematic.GetAdjacent();
+        return new SolutionResult(adjacent.Sum(_ => _.Value).ToString());
+
+    }
+
+    [Solveable("2023/Puzzles/Day3.txt", "Day 3 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var schematic = ParseInput(filename);
+        var adjacent = schematic.GetAdjacent();
+        var stars = schematic.Symbols.Where(_ => _.C == '*' && _.AdjacentTo.Count == 2);
+        var result = stars.Sum(_ => _.AdjacentTo.Select(_ => _.Value).Aggregate((a,b) => a*b));
+        return new SolutionResult(result.ToString());
+    }
+
+    public record Schematic(List<Part> Parts, List<Symbol> Symbols)
+    {
+        public List<Part> GetAdjacent()
+        {
+            var non = new List<Part>();
+            var adjacent = new List<Part>();
+            foreach (var part in Parts)
+            {
+                for (var i = 0; i < part.Width; i++)
+                {
+                    var x = part.X + i;
+                    var y = part.Y;
+                    var symbol = Symbols.FirstOrDefault(symbol => Math.Abs(symbol.X - x) <= 1 && Math.Abs(symbol.Y - y) <= 1);
+                    if (symbol != null)
+                    {
+                        adjacent.Add(part);
+                        symbol.AdjacentTo.Add(part);
+                        break;
+                    }
+                }
+            }
+            return adjacent;
+        }
+    }
+    public record Part(int X, int Y, int Width, int Value) { }
+    public record Symbol(char C, int X, int Y, List<Part> AdjacentTo) { }
+}
