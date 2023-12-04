721ebe1a64ef6ff0612cd5cc52f090cf9a342853
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-04 07:01:01 +0100

    2023 day 4

---
+++ 2023/2023.Tests/Day4Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC2023.Tests;
+public class Day4Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day4Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day4-test.txt";
+
+        //When
+        var result = Day4.ParseInput(filename);
+
+        //Then
+        Assert.True(6 == result.Count, $"Expected 6 but was {result.Count}");
+        Assert.True(result.First().WinningNumbers.Contains(17), $"Expected 17 to win but was missing");
+        Assert.True(result.First().PlayerNumbers.Contains(9), $"Expected 9 to be a player number but was missing");
+        Assert.True(result.Last().WinningNumbers.Contains(31), $"Expected 31 to win but was missing");
+        Assert.True(result.Last().PlayerNumbers.Contains(10), $"Expected 10 to be a player number but was missing");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day4-test.txt";
+
+        //When
+        var result = Day4.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("13" == result.Result, $"Expected 13 but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day4-test.txt";
+
+        //When
+        var result = Day4.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("30" == result.Result, $"Exptected 30 but was {result.Result}");
+    }
+
+}
---
+++ 2023/2023/Day4.cs
@@ -0,0 +1,59 @@
+﻿namespace AoC2023;
+public class Day4
+{
+    public static List<Card> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var result = new List<Card>();
+        foreach (var l in lines)
+        {
+            var sections = l.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            var cardNumber = int.Parse(sections.First().Split(" ").Last());
+            var winning = sections.Last().Split("|", StringSplitOptions.RemoveEmptyEntries).First().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(_ => int.Parse(_)).ToList();
+            var player = sections.Last().Split("|", StringSplitOptions.RemoveEmptyEntries).Last().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(_ => int.Parse(_)).ToList();
+            result.Add(new Card(cardNumber, winning, player));
+        }
+        return result;
+    }
+
+    [Solveable("2023/Puzzles/Day4.txt", "Day 4 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var cards = ParseInput(filename);
+        var sum = 0;
+        foreach (var card in cards)
+        {
+            var playerWin = card.PlayerNumbers.Where(_ => card.WinningNumbers.Contains(_));
+            if (playerWin.Any())
+            {
+                var count = playerWin.Count();
+                var cardSum = 1;
+                for (int i = 1; i < count; i++)
+                {
+                    cardSum *= 2;
+                }
+                sum += cardSum;
+            }
+        }
+        return new SolutionResult(sum.ToString());
+    }
+
+    [Solveable("2023/Puzzles/Day4.txt", "Day 4 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var cards = ParseInput(filename);
+        var remaining = new Dictionary<int, int>();
+        cards.ForEach(_ => remaining.Add(_.Number, 1));
+        foreach (var card in cards)
+        {
+            var winCount = card.PlayerNumbers.Where(_ => card.WinningNumbers.Contains(_)).Count();
+            for (int i = 1; i < winCount + 1; i++)
+            {
+                remaining[card.Number + i] += remaining[card.Number];
+            }
+        }
+        return new SolutionResult(remaining.Sum(_ => _.Value).ToString());
+    }
+
+    public record Card(int Number, List<int> WinningNumbers, List<int> PlayerNumbers) { }
+}
