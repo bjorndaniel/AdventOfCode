2579c999020ac90ed1305f0bf6e2f59060d2603d
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-06 07:07:12 +0100

    2023 Day 6

---
+++ 2023/2023.Tests/Day6Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC2023.Tests;
+public class Day6Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day6Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day6-test.txt";
+
+        //When
+        var result = Day6.ParseInput(filename);
+
+        //Then
+        Assert.True(3 == result.Count, $"Expected 3 but was {result.Count}");
+        Assert.True(7 == result.First().Time, $"Expected 7 but was {result.First().Time}");
+        Assert.True(9 == result.First().Record, $"Expected 9 but was {result.First().Record}");
+        Assert.True(30 == result.Last().Time, $"Expected 30 but was {result.Last().Time}");
+        Assert.True(200 == result.Last().Record, $"Expected 200 but was {result.Last().Record}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day6-test.txt";
+
+        //When
+        var result = Day6.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("288" == result.Result, $"Expected 288 but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day6-test.txt";
+
+        //When
+        var result = Day6.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("71503" == result.Result, $"Expected 71503 but was {result.Result}");
+    }
+
+}
---
+++ 2023/2023/Day6.cs
@@ -0,0 +1,63 @@
+﻿namespace AoC2023;
+public class Day6
+{
+    public static List<Race> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var result = new List<Race>();
+        var times = lines.First().Split(":").Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        var records = lines.Last().Split(":").Last().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < times.Length; i++)
+        {
+            result.Add(new Race(long.Parse(times[i]), long.Parse(records[i])));
+        }
+        return result;
+    }
+
+    [Solveable("2023/Puzzles/Day6.txt", "Day 6 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var races = ParseInput(filename);
+        var maxWays = new List<int>();
+        foreach (var race in races)
+        {
+            int maxCombinations = GetCombinations(race);
+            maxWays.Add(maxCombinations);
+        }
+        return new SolutionResult(maxWays.Aggregate((a, b) => a * b).ToString());
+    }
+
+    [Solveable("2023/Puzzles/Day6.txt", "Day 6 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var races = ParseInput(filename);
+        var ltime = long.Parse(races.Select(_ => _.Time.ToString()).Aggregate((a, b) => $"{a}{b}"));
+        var lrecord = long.Parse(races.Select(_ => _.Record.ToString()).Aggregate((a, b) => $"{a}{b}"));
+        var race = new Race(ltime, lrecord);
+        var time = race.Time;
+        var record = race.Record;
+        return new SolutionResult(GetCombinations(race).ToString());
+
+    }
+
+    private static int GetCombinations(Race race)
+    {
+        var time = race.Time;
+        var record = race.Record;
+        var maxCombinations = 0;
+        for (var waitTime = 0; waitTime < time; waitTime++)
+        {
+            var remainingTime = time - waitTime;
+            var distance = waitTime * remainingTime;
+            if (distance > record)
+            {
+                maxCombinations++;
+            }
+        }
+        return maxCombinations;
+    }
+
+
+    public record Race(long Time, long Record) { }
+
+}
