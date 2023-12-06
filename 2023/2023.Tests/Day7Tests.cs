3fc96d45e50251e31ae7b5e86da17b75f246e7e4
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-06 11:01:53 +0100

    Preparation for Day 7

---
+++ 2023/2023.Tests/Day7Tests.cs
@@ -0,0 +1,50 @@
+﻿namespace AoC2023.Tests;
+public class Day7Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day7Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+
+        //When
+        var result = Day7.ParseInput(filename);
+
+        //Then
+        Assert.True(false);
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+
+        //When
+        var result = Day7.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True(false);
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+
+        //When
+        var result = Day7.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True(false);
+    }
+
+}
---
+++ 2023/2023/Day7.cs
@@ -0,0 +1,23 @@
+﻿namespace AoC2023;
+public class Day7
+{
+    public static List<string> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return lines.ToList();
+    }
+
+    [Solveable("2023/Puzzles/Day7.txt", "Day 7 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        return new SolutionResult("");
+    }
+
+    [Solveable("2023/Puzzles/Day7.txt", "Day 7 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        return new SolutionResult("");
+    }
+
+
+}
