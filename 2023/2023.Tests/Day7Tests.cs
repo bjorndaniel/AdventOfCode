2792057f730547f96b55fefa62684130d49f0be7
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-07 11:31:43 +0100

    2023 Day 7

--- 2023/2023.Tests/Day7Tests.cs
+++ 2023/2023.Tests/Day7Tests.cs
@@ -18,33 +18,108 @@ public class Day7Tests
         var result = Day7.ParseInput(filename);
 
         //Then
-        Assert.True(false);
+        Assert.True(5 == result.Count, $"Expected 5 but was {result.Count}");
+        Assert.True('3' == result[0].Cards[0], $"Expected 3 but was {result[0].Cards[0]}");
+        Assert.True(HandType.Pair == result[0].Type, $"Expected Pair but was {result[0].Type}");
+        Assert.True(HandType.ThreeOfAKind == result[1].Type, $"Expected ThreeOfAKind    but was {result[1].Type}");
+        Assert.True(HandType.TwoPair == result[2].Type, $"Expected TwoPair but was {result[2].Type}");
+        Assert.True(HandType.TwoPair == result[3].Type, $"Expected TwoPair but was {result[3].Type}");
+        Assert.True(HandType.ThreeOfAKind == result[4].Type, $"Expected ThreeOfAKind but was {result[4].Type}");
     }
 
-    [Fact]
-    public void Can_solve_part1_for_test()
+    [Theory]
+    [InlineData("Day7-test.txt", "6440")]
+    [InlineData("Day7-test2.txt", "1343")]
+    public void Can_solve_part1_for_test(string input, string expected)
     {
         //Given
-        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+        var filename = $"{Helpers.DirectoryPathTests}{input}";
 
         //When
         var result = Day7.Part1(filename, new TestPrinter(_output));
 
         //Then
-        Assert.True(false);
+        Assert.True(expected == result.Result, $"Expected {expected} but was {result.Result}");
     }
 
-    [Fact]
-    public void Can_solve_part2_for_test()
+    [Theory]
+    [InlineData("Day7-test3.txt", "5905")]
+    [InlineData("Day7-test4.txt", "3667")]
+    public void Can_solve_part2_for_test(string input, string expected)
     {
         //Given
-        var filename = $"{Helpers.DirectoryPathTests}Day7-test.txt";
+        var filename = $"{Helpers.DirectoryPathTests}{input}";
 
         //When
         var result = Day7.Part2(filename, new TestPrinter(_output));
 
         //Then
-        Assert.True(false);
+        Assert.True(expected == result.Result, $"Expected {expected} but was {result.Result}");
     }
 
+    [Theory]
+    [InlineData("AAAAA", HandType.FiveOfAKind)]
+    [InlineData("AA8AA", HandType.FourOfAKind)]
+    [InlineData("23332", HandType.FullHouse)]
+    [InlineData("TTT98", HandType.ThreeOfAKind)]
+    [InlineData("23432", HandType.TwoPair)]
+    [InlineData("23456", HandType.HighCard)]
+    public void Can_get_hand_type(string cards, HandType expected)
+    {
+        //Given
+        var hand = cards.ToArray();
+
+        //When
+        var result = Day7.GetHandType(hand);
+
+        //Then
+        Assert.True(expected == result, $"Expected {expected} but was {result}");
+    }
+
+    [Fact]
+    public void Can_compare_hands()
+    {
+        var hand1 = new Hand(['2', 'A', 'A', 'A', 'A'], 0, 0, HandType.FourOfAKind);
+        var hand2 = new Hand(['A', 'K', 'K', 'K', 'K'], 0, 0, HandType.FourOfAKind);
+        var hands = new List<Hand> { hand1, hand2 };
+        var result = hands.OrderBy(_ => _).ToList();
+        Assert.True(hand2 == result[1]);
+        hand1 = new Hand(['5', '2', '3', '4', '6'], 0, 0, HandType.HighCard);
+        hand2 = new Hand(['2', '3', '4', '5', '6'], 0, 0, HandType.HighCard);
+        var hand3 = new Hand(['5', '4', '3', '2', '1'], 0, 0, HandType.HighCard);
+        hands = new List<Hand> { hand1, hand2, hand3 };
+        result = hands.OrderBy(_ => _).ToList();
+        Assert.True(hand2 == result[0]);
+        Assert.True(hand1 == result[1]);
+        Assert.True(hand3 == result[2]);
+        hand1 = new Hand(['2', '3', '4', '5', 'J'], 0, 0, HandType.FourOfAKind);
+        hand2 = new Hand(['J', '3', '4', '5', 'A'], 0, 0, HandType.FourOfAKind);
+        hands = new List<Hand> { hand1, hand2 };
+        result = hands.OrderBy(_ => _).ToList();
+        Assert.True(hand2 == result[1]);
+    }
+
+    [Fact]
+    public void Can_update_type()
+    {
+        var hand1 = new Hand(['3', '2', 'T', '3', 'K'], 0, 0, HandType.Pair);
+        hand1.UpdateType();
+        Assert.True(HandType.Pair == hand1.Type, $"Expected Pair but was {hand1.Type}");
+        hand1 = new Hand(['T', '5', '5', 'J', '5'], 0, 0, HandType.ThreeOfAKind);
+        hand1.UpdateType();
+        Assert.True(HandType.FourOfAKind == hand1.Type, $"Expected FourOfAKind but was {hand1.Type}");
+        hand1 = new Hand(['K', 'K', '6', '7', '7'], 0, 0, HandType.TwoPair);
+        hand1.UpdateType();
+        Assert.True(HandType.TwoPair == hand1.Type, $"Expected TwoPair but was {hand1.Type}");
+        hand1 = new Hand(['Q', 'Q', 'Q', 'J', 'A'], 0, 0, HandType.ThreeOfAKind);
+        hand1.UpdateType();
+        Assert.True(HandType.FourOfAKind == hand1.Type, $"Expected FourOfAKind but was {hand1.Type}");
+        hand1 = new Hand(['Q', 'Q', 'Q', 'J', 'J'], 0, 0, HandType.FullHouse);
+        hand1.UpdateType();
+        Assert.True(HandType.FiveOfAKind == hand1.Type, $"Expected FiveOfAKind but was {hand1.Type}");
+        hand1 = new Hand(['J', 'J', 'J', 'J', 'J'], 0, 0, HandType.FiveOfAKind);
+        hand1.UpdateType();
+        Assert.True(HandType.FiveOfAKind == hand1.Type, $"Expected FiveOfAKind but was {hand1.Type}");
+
+    }
 }
--- 2023/2023/Day7.cs
+++ 2023/2023/Day7.cs
@@ -1,23 +1,153 @@
 ﻿namespace AoC2023;
 public class Day7
 {
-    public static List<string> ParseInput(string filename)
+
+    public static List<Hand> ParseInput(string filename)
     {
         var lines = File.ReadAllLines(filename);
-        return lines.ToList();
+        var result = new List<Hand>();
+        foreach (var line in lines)
+        {
+            var cards = line.Split(" ");
+            var bid = int.Parse(cards[1]);
+            var hand = new Hand(cards[0].ToArray(), bid, 0, GetHandType(cards[0].ToArray()));
+            result.Add(hand);
+        }
+        return result;
+
     }
 
     [Solveable("2023/Puzzles/Day7.txt", "Day 7 part 1")]
     public static SolutionResult Part1(string filename, IPrinter printer)
     {
-        return new SolutionResult("");
+        var hands = ParseInput(filename).GroupBy(_ => _.Type).OrderBy(_ => _.Key);
+        var currentRank = 1;
+        foreach (var h in hands)
+        {
+            if (h.Count() == 1)
+            {
+                h.First().Rank = currentRank;
+                currentRank++;
+                continue;
+            }
+            var sortedHands = h.OrderBy(h => h);
+            foreach (var hand in sortedHands)
+            {
+                hand.Rank = currentRank;
+                currentRank++;
+            }
+        }
+        var ranked = hands.SelectMany(h => h).OrderByDescending(h => h.Rank);
+        return new SolutionResult(ranked.Sum(_ => _.Rank * _.Bid).ToString());
     }
 
     [Solveable("2023/Puzzles/Day7.txt", "Day 7 part 2")]
     public static SolutionResult Part2(string filename, IPrinter printer)
     {
-        return new SolutionResult("");
+        var _order = new char[] { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' };
+        var parsed = ParseInput(filename);
+        parsed.ForEach(_ => _.Ordering = _order);
+        parsed.ForEach(_ => _.UpdateType());
+        var hands = parsed.GroupBy(_ => _.Type).OrderBy(_ => _.Key);
+        var currentRank = 1;
+        foreach (var h in hands)
+        {
+            if (h.Count() == 1)
+            {
+                h.First().Rank = currentRank;
+                currentRank++;
+                continue;
+            }
+            var sortedHands = h.OrderBy(h => h);
+            foreach (var hand in sortedHands)
+            {
+                hand.Rank = currentRank;
+                currentRank++;
+            }
+        }
+        var ranked = hands.SelectMany(h => h).OrderBy(h => h.Rank);
+        return new SolutionResult(ranked.Sum(_ => _.Rank * _.Bid).ToString());
+
     }
 
+    public static HandType GetHandType(char[] cards)
+    {
+        var groups = cards.GroupBy(c => c);
+        var count = groups.Count();
+        return count switch
+        {
+            5 => HandType.HighCard,
+            4 => HandType.Pair,
+            3 => groups.Any(g => g.Count() == 3) ? HandType.ThreeOfAKind : HandType.TwoPair,
+            2 => groups.Any(g => g.Count() == 4) ? HandType.FourOfAKind : HandType.FullHouse,
+            _ => HandType.FiveOfAKind,
+        };
+    }
+
+    public class Hand : IComparable
+    {
+        private static readonly char[] _order = { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+        public Hand(char[] cards, int bid, int rank, HandType type)
+        {
+            Cards = cards;
+            Bid = bid;
+            Rank = rank;
+            Type = type;
+        }
+        public int Bid { get; set; }
+        public int Rank { get; set; }
+        public char[] Cards { get; set; }
+        public HandType Type { get; set; }
+        public char[] Ordering { get; set; } = _order;
+
+        public void UpdateType()
+        {
+            if (!Cards.Contains('J'))
+            {
+                return;
+            }
+            var grouped = Cards.Where(_ => _ != 'J').GroupBy(c => c);
+            var highest = Type;
+            foreach (var g in grouped)
+            {
+                var newC = Cards.ToList();
+                newC.RemoveAll(c => c == 'J');
+                newC.AddRange(Enumerable.Range(0, Cards.Count(c => c == 'J')).Select(_ => g.Key));
+                var type = GetHandType(newC.ToArray());
+                highest = type > highest ? type : highest;
+            }
+            Type = highest;
+        }
+        int IComparable.CompareTo(object? obj)
+        {
+            if (obj is Hand hand)
+            {
+                for (int i = 0; i < Cards.Length; i++)
+                {
+                    if (Array.IndexOf(Ordering, Cards[i]) < Array.IndexOf(Ordering, hand.Cards[i]))
+                    {
+                        return 1;
+                    }
+                    else if (Array.IndexOf(Ordering, Cards[i]) > Array.IndexOf(Ordering, hand.Cards[i]))
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+    public enum HandType
+    {
+        HighCard,
+        Pair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind,
+    }
 
 }
