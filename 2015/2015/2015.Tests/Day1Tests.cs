a062af21cd597173a0c92de8d3f4a0bd46ad7ba6
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-09 10:32:10 +0100

    2015 Day 1

---
+++ 2015/2015/2015.Tests/Day1Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace AoC2015.Tests;
+public class Day1Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day1Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day1-test.txt";
+
+        //When
+        var result = Day1.ParseInput(filename);
+
+        //Then
+        Assert.True(5 == result.Count, $"Expected 7 but was {result.Count}");
+        Assert.True(')' == result.Last(), $"Expected ) but was {result.Last()}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day1-test.txt";
+
+        //When
+        var result = Day1.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("-1" == result.Result, $"Expected -1 but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day1-test.txt";
+
+        //When
+        var result = Day1.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("5" == result.Result, $"Expected 5 but was {result.Result}");
+    }
+
+}
---
+++ 2015/2015/2015/Day1.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC2015;
+public class Day1
+{
+    public static List<char> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return lines.First().ToList();
+    }
+
+    [Solveable("2015/Puzzles/Day1.txt", "Day 1 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var instructions = ParseInput(filename);
+        var floor = instructions.Count(_ => _ == '(') - instructions.Count(_ => _ == ')');
+        return new SolutionResult(floor.ToString());
+    }
+
+    [Solveable("2015/Puzzles/Day1.txt", "Day 1 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var instructions = ParseInput(filename);
+        var floor = 0;
+        var count = 0;
+        while(floor != -1)
+        {
+            floor += instructions[count] == '(' ? 1 : -1;
+            count++;
+        }
+        
+        return new SolutionResult(count.ToString());
+    }
+
+
+}
