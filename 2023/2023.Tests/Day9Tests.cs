0e9e7999cc8e89d80c2fce83ed9364383b930b04
Daniel Sandberg <bjorndaniel@gmail.com>
2023-12-09 09:53:45 +0100

    2023 Day 9

---
+++ 2023/2023.Tests/Day9Tests.cs
@@ -0,0 +1,54 @@
+﻿namespace AoC2023.Tests;
+public class Day9Tests
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day9Tests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day9-test.txt";
+
+        //When
+        var result = Day9.ParseInput(filename);
+
+        //Then
+        Assert.True(3 == result.Count, $"Expected 3 but was {result.Count}");
+        Assert.True(0 == result[0][0], $"Expected 0 but was {result[0][0]}");
+        Assert.True(15 == result[0].Last(), $"Expected 15 but was {result[0].Last()}");
+        Assert.True(10 == result.Last()[0], $"Expected 10 but was {result.Last()[0]}");
+        Assert.True(45 == result.Last().Last(), $"Expected 45 but was {result.Last().Last()}");
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day9-test.txt";
+
+        //When
+        var result = Day9.Part1(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("114" == result.Result, $"Exptected 114 but was {result.Result}");
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day9-test.txt";
+
+        //When
+        var result = Day9.Part2(filename, new TestPrinter(_output));
+
+        //Then
+        Assert.True("2" == result.Result, $"Expected 2 but was {result.Result}");
+    }
+
+}
---
+++ 2023/2023/Day9.cs
@@ -0,0 +1,68 @@
+﻿namespace AoC2023;
+public class Day9
+{
+    public static List<List<long>> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return lines.Select(_ => _.Split(" ").Select(_ => long.Parse(_)).ToList()).ToList();
+    }
+
+    [Solveable("2023/Puzzles/Day9.txt", "Day 9 part 1")]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var sequences = ParseInput(filename);
+        var sum = 0L;
+        foreach (var sequence in sequences)
+        {
+            var nextSequenses = GetSequences(sequence);
+            var ordered = nextSequenses.OrderBy(_ => _.Count()).Skip(1).ToList();
+            for (int i = 0; i < ordered.Count() - 1; i++)
+            {
+                ordered[i + 1].Add(ordered[i + 1].Last() + ordered[i].Last());
+            }
+            sum += ordered.Last().Last();
+        }
+
+        return new SolutionResult(sum.ToString());
+    }
+
+
+    [Solveable("2023/Puzzles/Day9.txt", "Day 9 part 2")]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var sequences = ParseInput(filename);
+        var sum = 0L;
+        foreach (var sequence in sequences)
+        {
+            var nextSequenses = GetSequences(sequence);
+            var ordered = nextSequenses.OrderBy(_ => _.Count()).Skip(1).ToList();
+            for (int i = 0; i < ordered.Count() - 1; i++)
+            {
+                ordered[i + 1].Insert(0,ordered[i + 1].First() - ordered[i].First());
+            }
+            sum += ordered.Last().First();
+        }
+        return new SolutionResult(sum.ToString());
+    }
+
+    private static List<List<long>> GetSequences(List<long> sequence)
+    {
+        var nextSequenses = new List<List<long>>
+            {
+                sequence
+            };
+        var current = sequence;
+        while (!current.All(_ => _ == 0))
+        {
+            var newSequence = new List<long>();
+            for (int i = 1; i < current.Count; i++)
+            {
+                newSequence.Add(current[i] - current[i - 1]);
+            }
+            nextSequenses.Add(newSequence);
+            current = newSequence;
+        }
+
+        return nextSequenses;
+    }
+}
