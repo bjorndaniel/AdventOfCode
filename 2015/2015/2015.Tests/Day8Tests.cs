58dddfab679453968217250867e47fa675056b0f
Daniel Sandberg <bjorndaniel@gmail.com>
2024-12-01 15:55:17 +0100

    2015 Day 8

---
+++ 2015/2015/2015.Tests/Day8Tests.cs
@@ -0,0 +1,47 @@
+﻿namespace AoC2015.Tests;
+public class Day8Tests(ITestOutputHelper output)
+{
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.ParseInput(filename);
+
+        //Then
+        Assert.Equal(4, result.Count);
+        Assert.Equal("\"\"", result[0]);
+        Assert.Equal("\"abc\"", result[1]);
+
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.Part1(filename, new TestPrinter(output));
+
+        //Then
+        Assert.Equal("12", result.Result);
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day8-test.txt";
+
+        //When
+        var result = Day8.Part2(filename, new TestPrinter(output));
+
+        //Then
+        Assert.Equal("19", result.Result);
+    }
+
+}
---
+++ 2015/2015/2015/Day8.cs
@@ -0,0 +1,48 @@
+﻿namespace AoC2015;
+public class Day8
+{
+    public static List<string> ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        return [.. lines];
+    }
+
+    [Solveable("2015/Puzzles/Day8.txt", "Day 8 part 1", 8)]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var input = ParseInput(filename);
+        int totalCharacters = 0;
+        foreach (var line in input)
+        {
+            var unquoted = line.Substring(1, line.Length - 2);
+            var decoded = Regex.Replace(unquoted, @"\\(\\|""|x[0-9a-fA-F]{2})", match =>
+            {
+                if (match.Value.StartsWith("\\x"))
+                {
+                    return ((char)Convert.ToInt32(match.Value.Substring(2), 16)).ToString();
+                }
+                else
+                {
+                    return match.Value.Substring(1);
+                }
+            });
+            totalCharacters += line.Length - decoded.Length;
+        }
+        return new SolutionResult(totalCharacters.ToString());
+    }
+
+    [Solveable("2015/Puzzles/Day8.txt", "Day 8 part 2", 8)]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var input = ParseInput(filename);
+        int totalCharacters = 0;
+        foreach (var line in input)
+        {
+            var encoded = "\"" + line.Replace("\\", "\\\\").Replace("\"", "\\\"") + "\"";
+            totalCharacters += encoded.Length - line.Length;
+        }
+        return new SolutionResult(totalCharacters.ToString());
+    }
+
+
+}
