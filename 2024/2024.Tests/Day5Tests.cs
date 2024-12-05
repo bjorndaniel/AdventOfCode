77c640748fd56ac6f49979d4dea0bcc6bb303b0c
Daniel Sandberg <bjorndaniel@gmail.com>
2024-12-05 06:56:23 +0100

    2024 Day 5

---
+++ 2024/2024.Tests/Day5Tests.cs
@@ -0,0 +1,49 @@
+﻿namespace AoC2024.Tests;
+public class Day5Tests(ITestOutputHelper output)
+{
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+        //When
+        var result = Day5.ParseInput(filename);
+
+        //Then
+        Assert.Equal(21, result.rules.Count);
+        Assert.Equal(6, result.instructions.Count);
+        Assert.Equal((47, 53), result.rules.First());
+        Assert.Equal((53, 13), result.rules.Last());
+        Assert.Equal(new List<int> { 75, 47, 61, 53, 29 }, result.instructions.First());
+        Assert.Equal(new List<int> { 97, 13, 75, 29, 47 }, result.instructions.Last());
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+        //When
+        var result = Day5.Part1(filename, new TestPrinter(output));
+
+        //Then
+        Assert.Equal("143", result.Result);
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day5-test.txt";
+
+        //When
+        var result = Day5.Part2(filename, new TestPrinter(output));
+
+        //Then
+        Assert.Equal("123", result.Result);
+    }
+
+}
---
+++ 2024/2024/Day5.cs
@@ -0,0 +1,111 @@
+﻿namespace AoC2024;
+public class Day5
+{
+    public static (List<(int first, int second)> rules, List<List<int>> instructions) ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var instructions = new List<List<int>>();
+        var rules = new List<(int first, int second)>();
+        foreach (var line in lines)
+        {
+            if (line.Contains("|"))
+            {
+                var ruleParts = line.Split("|");
+                rules.Add((int.Parse(ruleParts[0]), int.Parse(ruleParts[1])));
+            }
+            else if (line.Contains(","))
+            {
+                var instructionParts = line.Split(",");
+                instructions.Add(instructionParts.Select(_ => int.Parse(_)).ToList());
+            }
+        }
+        return (rules, instructions);
+    }
+
+    [Solveable("2024/Puzzles/Day5.txt", "Day 5 part 1", 3)]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var (rules, instructions) = ParseInput(filename);
+        var result = 0;
+        foreach (var instruction in instructions)
+        {
+            var middleIndex = instruction.Count / 2;
+            var number = instruction[middleIndex];
+            var isValid = true;
+            foreach (var rule in rules)
+            {
+                var firstIndex = instruction.IndexOf(rule.first);
+                var secondIndex = instruction.IndexOf(rule.second);
+                if (firstIndex == -1 || secondIndex == -1)
+                {
+                    continue;
+                }
+                if (firstIndex > secondIndex)
+                {
+                    isValid = false;
+                }
+            }
+            if (isValid)
+            {
+                result += number;
+            }
+        }
+        return new SolutionResult(result.ToString());
+    }
+
+    [Solveable("2024/Puzzles/Day5.txt", "Day 5 part 2", 3)]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        var (rules, instructions) = ParseInput(filename);
+        var result = 0;
+        var invalidInstructions = new List<List<int>>();
+        foreach (var instruction in instructions)
+        {
+            if (IsOrdered(instruction).isOrdered is false)
+            {
+                invalidInstructions.Add(instruction);
+            }
+        }
+
+        foreach (var instruction in invalidInstructions)
+        {
+            var (isOrdered, firstIndex, secondIndex) = IsOrdered(instruction);
+            while (isOrdered is false)
+            {
+                var temp = instruction[firstIndex];
+                instruction[firstIndex] = instruction[secondIndex];
+                instruction[secondIndex] = temp;
+                (isOrdered, firstIndex, secondIndex) = IsOrdered(instruction);
+            }
+        }
+
+        foreach(var instruction in invalidInstructions)
+        {
+            var middleIndex = instruction.Count / 2;
+            var number = instruction[middleIndex];
+            result += number;
+        }
+
+        return new SolutionResult(result.ToString());
+
+        (bool isOrdered, int firstIndex, int secondIndex) IsOrdered(List<int> instruction)
+        {
+            foreach (var rule in rules)
+            {
+                var firstIndex = instruction.IndexOf(rule.first);
+                var secondIndex = instruction.IndexOf(rule.second);
+                if (firstIndex == -1 || secondIndex == -1)
+                {
+                    continue;
+                }
+                if (firstIndex > secondIndex)
+                {
+                    return (false, firstIndex, secondIndex);
+                }
+            }
+            return (true, 0, 0);
+        }
+    }
+
+
+}
