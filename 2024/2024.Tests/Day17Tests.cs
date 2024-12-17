1fdb020d8d0662a9851c631cc2ea1b3d2bcf91fa
Daniel Sandberg <bjorndaniel@gmail.com>
2024-12-17 06:54:07 +0100

    2024 Day 17 in progress

---
+++ 2024/2024.Tests/Day17Tests.cs
@@ -0,0 +1,49 @@
+﻿namespace AoC2024.Tests;
+public class Day17Tests(ITestOutputHelper output)
+{
+
+    [Fact]
+    public void Can_parse_input()
+    {
+        //Given 
+        var filename = $"{Helpers.DirectoryPathTests}Day17-test.txt";
+
+        //When
+        var (computer, program) = Day17.ParseInput(filename);
+
+        //Then
+        Assert.Equal(729, computer.A);
+        Assert.Equal(0, computer.B);
+        Assert.Equal(0, computer.C);
+        Assert.Equal(6, program.Count());
+        Assert.Equal(0, program.First());
+        Assert.Equal(5, program[2]);
+    }
+
+    [Fact]
+    public void Can_solve_part1_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day17-test.txt";
+
+        //When
+        var result = Day17.Part1(filename, new TestPrinter(output));
+
+        //Then
+        Assert.Equal("4,6,3,5,6,3,5,2,1,0", result.Result);
+    }
+
+    [Fact]
+    public void Can_solve_part2_for_test()
+    {
+        //Given
+        var filename = $"{Helpers.DirectoryPathTests}Day17-test.txt";
+
+        //When
+        var result = Day17.Part2(filename, new TestPrinter(output));
+
+        //Then
+        Assert.True(false);
+    }
+
+}
---
+++ 2024/2024/Day17.cs
@@ -0,0 +1,164 @@
+﻿namespace AoC2024;
+public class Day17
+{
+    public static (Computer computer, List<int> program) ParseInput(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var regA = 0L;
+        var regB = 0L;
+        var regC = 0L;
+        var program = new List<int>();
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i < 3)
+            {
+                var vals = lines[i].Split(":");
+                if (i == 0)
+                {
+                    regA = long.Parse(vals[1]);
+                }
+                if (i == 1)
+                {
+                    regB = long.Parse(vals[1]);
+                }
+                if (i == 2)
+                {
+                    regC = long.Parse(vals[1]);
+                }
+            }
+            if (i == 4)
+            {
+                var instr = lines[i].Split(":");
+                program = instr[1].Split(",").Select(_ => int.Parse(_.Trim())).ToList();
+            }
+        }
+        return (new Computer(regA, regB, regC), program);
+    }
+
+    [Solveable("2024/Puzzles/Day17.txt", "Day 17 part 1", 17)]
+    public static SolutionResult Part1(string filename, IPrinter printer)
+    {
+        var (computer, program) = ParseInput(filename);
+        var outputs = new List<long>();
+        var pointer = 0;
+        while(pointer < program.Count)
+        {
+            var instr = (Instruction)program[pointer];
+            var operand = (ComboOperand)program[pointer + 1];
+            var (output, jump) = computer.RunInstruction(instr, operand);
+            if (output.HasValue)
+            {
+                outputs.Add(output.Value);
+            }
+            if (jump.HasValue)
+            {
+                pointer = jump.Value;
+            }
+            else
+            {
+                jump += 2;
+            }
+        }
+        
+        return new SolutionResult(outputs.Select(_ => _.ToString()).Aggregate((a, b) => $"{a},{b}"));
+    }
+
+    [Solveable("2024/Puzzles/Day17.txt", "Day 17 part 2", 17)]
+    public static SolutionResult Part2(string filename, IPrinter printer)
+    {
+        return new SolutionResult("");
+    }
+
+
+}
+
+public class Computer(long a, long b, long c)
+{
+    public long A { get; set; } = a;
+    public long B { get; set; } = b;
+    public long C { get; set; } = c;
+
+    public (long? result, int? jump) RunInstruction(Instruction instruction, ComboOperand operand)
+    {
+        switch (instruction)
+        {
+            case Instruction.Adv:
+                var numerator = A;
+                var denominator = GetOperand(operand);
+                var result = numerator / denominator;
+                A = (int)result;
+                break;
+            case Instruction.Bxl:
+                var op = (int)operand;
+                B = B ^ op;
+                break;
+            case Instruction.Bst:
+                B = ((int)operand) % 8;
+                break;
+            case Instruction.Jnz:
+                if(A != 0)
+                {
+                    return (null, (int)operand);    
+                }
+                break;
+            case Instruction.Bxc:
+                B = B ^ C;
+                break;
+            case Instruction.Out:
+                var value = ((int)operand) % 8;
+                return (value, null);
+            case Instruction.Bdv:
+                var numerator = A;
+                var denominator = GetOperand(operand);
+                var result = numerator / denominator;
+                B = (int)result;
+                break;
+            case Instruction.Cdv:
+                var numerator = A;
+                var denominator = GetOperand(operand);
+                var result = numerator / denominator;
+                C = (int)result;
+                break;
+        }
+
+        long GetOperand(ComboOperand operand)
+        {
+            return operand switch
+            {
+                ComboOperand.Literal0 => 0,
+                ComboOperand.Literal1 => 1,
+                ComboOperand.Literal2 => 2,
+                ComboOperand.Literal3 => 3,
+                ComboOperand.ValueA => A,
+                ComboOperand.ValueB => B,
+                ComboOperand.ValueC => C,
+                _ => throw new Exception("Invalid operand")
+            };
+        }
+
+    }
+}
+
+public enum ComboOperand
+{
+    Literal0,
+    Literal1,
+    Literal2,
+    Literal3,
+    ValueA,
+    ValueB,
+    ValueC,
+    Reserved
+}
+
+public enum Instruction
+{
+    Adv,
+    Bxl,
+    Bst,
+    Jnz,
+    Bxc,
+    Out,
+    Bdv,
+    Cdv
+}
